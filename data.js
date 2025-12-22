5f6a6b92681cb67a6026a745829600594ed34a9e
19961124yrl-dev <19961124yrl@gmail.com>
2025-12-22 16:44:02 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+window.EQUIPMENT_DATA = {
+    "dev_1766020159919": {
+        "name": "SST-AI100",
+        "modules": {
+            "mod_1766020168009": {
+                "name": "電気回路図",
+                "subcomponents": {
+                    "sub_1766020243609": {
+                        "name": "SST-回路図-20251002",
+                        "processes": [
+                            {
+                                "name": "SST-回路図-20251002",
+                                "pdf": "pdfs/SST-回路図-20251002.pdf"
+                            }
+                        ]
+                    }
+                }
+            },
+            "mod_1766020270305": {
+                "name": "出荷書類",
+                "subcomponents": {
+                    "sub_1766020306909": {
+                        "name": "WPH報告書",
+                        "processes": [
+                            {
+                                "name": "WPH報告書",
+                                "pdf": "pdfs/WPH_Report_SST-AI100_jp.pdf"
+                            }
+                        ]
+                    },
+                    "sub_1766020441923": {
+                        "name": "Utility",
+                        "processes": [
+                            {
+                                "name": "Utility",
+                                "pdf": "pdfs/S&STech-AI-100-Utility.pdf"
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }
+};
