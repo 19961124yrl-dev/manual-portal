fdd16db29cae96c1cb711056cca1484345f596ff
19961124yrl@gmail.com <19961124yrl@gmail.com>
2025-12-22 18:33:54 +0900

    20251222

--- data.js
+++ data.js
@@ -19,21 +19,338 @@ window.EQUIPMENT_DATA = {
             "mod_1766020270305": {
                 "name": "出荷書類",
                 "subcomponents": {
-                    "sub_1766020306909": {
-                        "name": "WPH報告書",
+                    "sub_1766395979362_88": {
+                        "name": "LODAS-AI100-SST様向け-出荷检查",
                         "processes": [
                             {
-                                "name": "WPH報告書",
-                                "pdf": "pdfs/WPH_Report_SST-AI100_jp.pdf"
+                                "name": "LODAS-AI100-SST様向け-出荷检查",
+                                "file": "pdfs/SST-AI100/LODAS-AI100-SST様向け-出荷检查.pdf",
+                                "ext": "pdf"
                             }
                         ]
                     },
-                    "sub_1766020441923": {
-                        "name": "Utility",
+                    "sub_1766395979362_564": {
+                        "name": "S&STech-AI-100-Utility",
                         "processes": [
                             {
-                                "name": "Utility",
-                                "pdf": "pdfs/S&STech-AI-100-Utility.pdf"
+                                "name": "S&STech-AI-100-Utility",
+                                "file": "pdfs/SST-AI100/S&STech-AI-100-Utility.pdf",
+                                "ext": "pdf"
+                            }
+                        ]
+                    },
+                    "sub_1766395979362_591": {
+                        "name": "SST-回路図-20251002",
+                        "processes": [
+                            {
+                                "name": "SST-回路図-20251002",
+                                "file": "pdfs/SST-AI100/SST-回路図-20251002.pdf",
+                                "ext": "pdf"
+                            }
+                        ]
+                    },
+                    "sub_1766395979362_945": {
+                        "name": "WPH_Report_SST-AI100_jp",
+                        "processes": [
+                            {
+                                "name": "WPH_Report_SST-AI100_jp",
+                                "file": "pdfs/SST-AI100/WPH_Report_SST-AI100_jp.pdf",
+                                "ext": "pdf"
+                            }
+                        ]
+                    }
+                }
+            },
+            "mod_1766393292359": {
+                "name": "出荷写真",
+                "subcomponents": {
+                    "sub_1766393883742_679": {
+                        "name": "0bcb3b14-9fb1-44cd-8c45-04bc9a976635",
+                        "processes": [
+                            {
+                                "name": "0bcb3b14-9fb1-44cd-8c45-04bc9a976635",
+                                "file": "images/SST-AI100/0bcb3b14-9fb1-44cd-8c45-04bc9a976635.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_601": {
+                        "name": "ccd36d50-3b41-4bbc-b00c-9aad06b1a1d0",
+                        "processes": [
+                            {
+                                "name": "ccd36d50-3b41-4bbc-b00c-9aad06b1a1d0",
+                                "file": "images/SST-AI100/ccd36d50-3b41-4bbc-b00c-9aad06b1a1d0.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_339": {
+                        "name": "11be498b-3ac3-4d9d-ad49-900a4354a808",
+                        "processes": [
+                            {
+                                "name": "11be498b-3ac3-4d9d-ad49-900a4354a808",
+                                "file": "images/SST-AI100/11be498b-3ac3-4d9d-ad49-900a4354a808.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_771": {
+                        "name": "130d262e-5407-41ba-b48f-ba5887ae788d",
+                        "processes": [
+                            {
+                                "name": "130d262e-5407-41ba-b48f-ba5887ae788d",
+                                "file": "images/SST-AI100/130d262e-5407-41ba-b48f-ba5887ae788d.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_512": {
+                        "name": "1f4beaa4-d50b-418c-8445-93805d76f13e",
+                        "processes": [
+                            {
+                                "name": "1f4beaa4-d50b-418c-8445-93805d76f13e",
+                                "file": "images/SST-AI100/1f4beaa4-d50b-418c-8445-93805d76f13e.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_294": {
+                        "name": "2509fcf5-82b5-4df4-85ce-22180470427f",
+                        "processes": [
+                            {
+                                "name": "2509fcf5-82b5-4df4-85ce-22180470427f",
+                                "file": "images/SST-AI100/2509fcf5-82b5-4df4-85ce-22180470427f.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_744": {
+                        "name": "2d2488c7-b1bc-46ac-ba31-4e5dc0f7a8e5",
+                        "processes": [
+                            {
+                                "name": "2d2488c7-b1bc-46ac-ba31-4e5dc0f7a8e5",
+                                "file": "images/SST-AI100/2d2488c7-b1bc-46ac-ba31-4e5dc0f7a8e5.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_517": {
+                        "name": "2ec76db8-4d56-4560-8f01-89afaec97636",
+                        "processes": [
+                            {
+                                "name": "2ec76db8-4d56-4560-8f01-89afaec97636",
+                                "file": "images/SST-AI100/2ec76db8-4d56-4560-8f01-89afaec97636.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_894": {
+                        "name": "439c8d02-be5a-463f-802d-98d6ce0177b3",
+                        "processes": [
+                            {
+                                "name": "439c8d02-be5a-463f-802d-98d6ce0177b3",
+                                "file": "images/SST-AI100/439c8d02-be5a-463f-802d-98d6ce0177b3.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_667": {
+                        "name": "4efe5642-9b41-44d8-890b-560341bf13f6",
+                        "processes": [
+                            {
+                                "name": "4efe5642-9b41-44d8-890b-560341bf13f6",
+                                "file": "images/SST-AI100/4efe5642-9b41-44d8-890b-560341bf13f6.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_552": {
+                        "name": "567fb14b-e74d-4487-a16d-19f99cf3d4fd",
+                        "processes": [
+                            {
+                                "name": "567fb14b-e74d-4487-a16d-19f99cf3d4fd",
+                                "file": "images/SST-AI100/567fb14b-e74d-4487-a16d-19f99cf3d4fd.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_371": {
+                        "name": "60c8c257-dd22-4a34-9f02-5b9ebb51a3fe",
+                        "processes": [
+                            {
+                                "name": "60c8c257-dd22-4a34-9f02-5b9ebb51a3fe",
+                                "file": "images/SST-AI100/60c8c257-dd22-4a34-9f02-5b9ebb51a3fe.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_806": {
+                        "name": "634b6192-509b-430f-b4b9-9c1dddad1ef4",
+                        "processes": [
+                            {
+                                "name": "634b6192-509b-430f-b4b9-9c1dddad1ef4",
+                                "file": "images/SST-AI100/634b6192-509b-430f-b4b9-9c1dddad1ef4.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_958": {
+                        "name": "ab07da6c-2229-4ac0-a5a4-8945c1220f9b",
+                        "processes": [
+                            {
+                                "name": "ab07da6c-2229-4ac0-a5a4-8945c1220f9b",
+                                "file": "images/SST-AI100/ab07da6c-2229-4ac0-a5a4-8945c1220f9b.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_721": {
+                        "name": "7c839edc-6c6c-47a4-b934-59c98af4b5d7",
+                        "processes": [
+                            {
+                                "name": "7c839edc-6c6c-47a4-b934-59c98af4b5d7",
+                                "file": "images/SST-AI100/7c839edc-6c6c-47a4-b934-59c98af4b5d7.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_872": {
+                        "name": "824625bc-cb44-4ea0-b4d8-e7bef1e25f69",
+                        "processes": [
+                            {
+                                "name": "824625bc-cb44-4ea0-b4d8-e7bef1e25f69",
+                                "file": "images/SST-AI100/824625bc-cb44-4ea0-b4d8-e7bef1e25f69.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_349": {
+                        "name": "930c7271-9f9d-42b5-8210-0c3d8405fd51",
+                        "processes": [
+                            {
+                                "name": "930c7271-9f9d-42b5-8210-0c3d8405fd51",
+                                "file": "images/SST-AI100/930c7271-9f9d-42b5-8210-0c3d8405fd51.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_569": {
+                        "name": "936f1845-15ea-4eb3-bd37-9defa3bd5790",
+                        "processes": [
+                            {
+                                "name": "936f1845-15ea-4eb3-bd37-9defa3bd5790",
+                                "file": "images/SST-AI100/936f1845-15ea-4eb3-bd37-9defa3bd5790.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_333": {
+                        "name": "974286b0-e9b1-4435-86ac-76ed34b3b2e7",
+                        "processes": [
+                            {
+                                "name": "974286b0-e9b1-4435-86ac-76ed34b3b2e7",
+                                "file": "images/SST-AI100/974286b0-e9b1-4435-86ac-76ed34b3b2e7.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_811": {
+                        "name": "9d7533a7-e58c-41e5-b522-f272e8a94d61",
+                        "processes": [
+                            {
+                                "name": "9d7533a7-e58c-41e5-b522-f272e8a94d61",
+                                "file": "images/SST-AI100/9d7533a7-e58c-41e5-b522-f272e8a94d61.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_318": {
+                        "name": "a6fc58b0-341d-46e9-9358-4dc50e7fedb5",
+                        "processes": [
+                            {
+                                "name": "a6fc58b0-341d-46e9-9358-4dc50e7fedb5",
+                                "file": "images/SST-AI100/a6fc58b0-341d-46e9-9358-4dc50e7fedb5.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_854": {
+                        "name": "a84a105f-0e02-4960-950a-10413b26f962",
+                        "processes": [
+                            {
+                                "name": "a84a105f-0e02-4960-950a-10413b26f962",
+                                "file": "images/SST-AI100/a84a105f-0e02-4960-950a-10413b26f962.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_485": {
+                        "name": "cbfddfc4-bea5-4839-bdda-4e4297f364bf",
+                        "processes": [
+                            {
+                                "name": "cbfddfc4-bea5-4839-bdda-4e4297f364bf",
+                                "file": "images/SST-AI100/cbfddfc4-bea5-4839-bdda-4e4297f364bf.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_662": {
+                        "name": "d5a900c3-e749-445d-b25e-1dd321fd28c0",
+                        "processes": [
+                            {
+                                "name": "d5a900c3-e749-445d-b25e-1dd321fd28c0",
+                                "file": "images/SST-AI100/d5a900c3-e749-445d-b25e-1dd321fd28c0.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_949": {
+                        "name": "da353cc6-c092-4e45-9a65-8d5ff8ac54b6",
+                        "processes": [
+                            {
+                                "name": "da353cc6-c092-4e45-9a65-8d5ff8ac54b6",
+                                "file": "images/SST-AI100/da353cc6-c092-4e45-9a65-8d5ff8ac54b6.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_451": {
+                        "name": "dc27c881-e1c8-4270-bce6-602f5c166a85",
+                        "processes": [
+                            {
+                                "name": "dc27c881-e1c8-4270-bce6-602f5c166a85",
+                                "file": "images/SST-AI100/dc27c881-e1c8-4270-bce6-602f5c166a85.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_928": {
+                        "name": "df1860aa-8a2a-434d-b141-6cf1d44dfc6c",
+                        "processes": [
+                            {
+                                "name": "df1860aa-8a2a-434d-b141-6cf1d44dfc6c",
+                                "file": "images/SST-AI100/df1860aa-8a2a-434d-b141-6cf1d44dfc6c.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_83": {
+                        "name": "f8c68447-1e49-4491-a547-834812db3744",
+                        "processes": [
+                            {
+                                "name": "f8c68447-1e49-4491-a547-834812db3744",
+                                "file": "images/SST-AI100/f8c68447-1e49-4491-a547-834812db3744.jpg",
+                                "ext": "jpg"
+                            }
+                        ]
+                    },
+                    "sub_1766393883742_497": {
+                        "name": "fd1ae831-2564-4491-96ab-44a0a50e9de6",
+                        "processes": [
+                            {
+                                "name": "fd1ae831-2564-4491-96ab-44a0a50e9de6",
+                                "file": "images/SST-AI100/fd1ae831-2564-4491-96ab-44a0a50e9de6.jpg",
+                                "ext": "jpg"
                             }
                         ]
                     }
